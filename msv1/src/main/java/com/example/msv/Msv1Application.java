1e5dcab0ee05baa55b04debc84dec6af421aad9d
yongli <2869486287@qq.com>
2019-05-26 14:21:07 +0800

    5/26

--- msv1/src/main/java/com/example/msv/Msv1Application.java
+++ msv1/src/main/java/com/example/msv/Msv1Application.java
@@ -2,11 +2,8 @@ package com.example.msv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.EnableMBeanExport;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Msv1Application {
 
 	public static void main(String[] args) {
